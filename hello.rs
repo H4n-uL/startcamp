4cec4e7ba358efa72445f28c03ebc829434ef562
H4n-uL <jun061119@gmail.com>
2025-07-17 15:31:43 +0900

    DOC :: How to use git

--- hello.rs
+++ hello.rs
@@ -1,3 +1,13 @@
 ghasdjk;lghask;jehfdsfdsoigsdnkjfjkdshk
 jl;
 jl
+
+git config --global user.name "USERNAME"
+git config --global user.email "E-MAIL"
+
+git init
+git remote add "REMOTE ALIAS" "URL"
+git add "FILE"
+git commit -m "COMMIT MESSAGE"
+git push "REMOTE ALIAS" "BRANCH"
+git pull "REMOTE ALIAS" "BRANCH"
